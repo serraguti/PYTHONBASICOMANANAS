e19891f10c8c67922900f3e21952725f3f52193f
serraguti <pacoserranox@gmail.com>
2025-06-24 13:59:36 +0200

    fin de martes

--- python07metodos.py
+++ python07metodos.py
@@ -5,9 +5,16 @@ def primerMetodo():
     print("Ejecutando metodo 1")
 def segundoMetodo():
     print("Ejecutando metodo 2")
+def saludar(nombre):
+    print(f"Hola en Python {nombre}")
+def despedirse(nombre, dia):
+    print(f"Hasta luego {nombre} en el día {dia}")
 #---------------------------------------
 #CODIGO PRINCIPAL DE NUESTRO PROGRAMA MAIN
 print("Ejemplo de métodos")
+saludar("Paco")
+despedirse("Paco", "Martes")
+#saludar("Paco")
 #LLAMADA A LOS METODOS
 primerMetodo()
 segundoMetodo()
