aa6bc0ce7219dc9d769127c55e641f035de20712
serraguti <pacoserranox@gmail.com>
2025-06-24 12:44:33 +0200

    bucles

--- python04bucles.py
+++ python04bucles.py
@@ -1,4 +1,14 @@
 print("Ejemplo de bucles")
+print("Conjetura de Collatz")
+numero = int(input("Introduzca un número: "))
+while (numero != 1):
+    if (numero % 2 == 0):
+        numero = int(numero / 2)
+    else:
+        numero = numero * 3 + 1
+    print(numero)
+print("-----------------------")
+
 #REALIZAMOS EL TIPICO BUCLE DE DIBUJAR PARES EN UN RANGO %
 for i in range(20):
     #Preguntamos si un número es Par o impar
