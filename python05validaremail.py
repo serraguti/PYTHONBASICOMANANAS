8606a2825f9482cc1896ba3b199bdb4fa8716dd3
serraguti <pacoserranox@gmail.com>
2025-06-24 13:00:18 +0200

    email string

---
+++ python05validaremail.py
@@ -0,0 +1,8 @@
+print("Validacion de email")
+email = input("Introduzca email: ")
+# •	Que el email contenga una @
+# •	Que el email contenga un punto
+# •	@ ni al inicio ni al final
+# •	Solamente una @ en el mail
+# •	Un punto después de la @
+# •	Dominio de 2 a 3 caracteres
