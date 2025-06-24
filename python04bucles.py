8a758f74155028af5c2de97ff25bd72bf471e062
serraguti <pacoserranox@gmail.com>
2025-06-24 12:13:39 +0200

    bucles for

---
+++ python04bucles.py
@@ -0,0 +1,7 @@
+print("Ejemplo de bucles")
+#REALIZAMOS EL TIPICO BUCLE DE DIBUJAR PARES EN UN RANGO %
+for i in range(20):
+    #Preguntamos si un número es Par o impar
+    if (i % 2 == 0):
+        print(f"Par: {i}")
+print("Fin de programa")
